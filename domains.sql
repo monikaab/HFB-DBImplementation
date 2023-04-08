5f930d59f3d360d87731489645b068c2ae299141
akkpuvvada <akk.puvvada@gmail.com>
2023-04-07 21:32:14 -0500

    Adding domain for email

---
+++ domains.sql
@@ -0,0 +1,3 @@
+CREATE DOMAIN email_domain char(30)
+	CONSTRAINT email_test 
+	CHECK( VALUE ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$');

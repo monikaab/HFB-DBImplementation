fcb48ecfafd8c60961af8861128b60e6d2fd1340
akkpuvvada <akk.puvvada@gmail.com>
2023-04-08 01:02:58 -0500

    Adding encryption for password

--- tables/createtables.sql
+++ tables/createtables.sql
@@ -1,8 +1,21 @@
 CREATE TABLE person (
   ID VARCHAR(10),
-  name VARCHAR(20) NOT NULL,
+  name name_domain,
   Email email_domain,
-  DOB DATE NOT NULL,
-  sex VARCHAR(10) NOT NULL,
+  DOB DOB,
+  sex sex_domain,
   PRIMARY KEY (ID)
+);
+
+CREATE EXTENSION pgcrypto;
+
+CREATE TABLE employee (
+	ID VARCHAR(10),
+	name name_domain,
+	email email_domain,
+	DOB DOB,
+  	sex sex_domain,
+	username varchar(20),
+	password TEXT NOT NULL,
+	FOREIGN KEY (id) REFERENCES person
 );
